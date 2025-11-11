5be001bc51f0988881da21602f6c3ffc89e5fc06
ifedips20s <ifedips20s@gmail.com>
2025-11-11 12:21:43 +0100

    Add interfaces/index.ts

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -1,16 +1,22 @@
+import React from "react";
 import { ButtonProps } from "@/interfaces";
 
-const sizeClasses = {
-  sm: "px-3 py-1 text-sm",
-  md: "px-4 py-2 text-base",
-  lg: "px-6 py-3 text-lg",
-};
+const Button: React.FC<ButtonProps> = ({ title, size = "md", shape = "rounded-md", styles = "" }) => {
+  let sizeClasses = "";
+  switch (size) {
+    case "sm":
+      sizeClasses = "px-3 py-1 text-sm";
+      break;
+    case "md":
+      sizeClasses = "px-4 py-2 text-base";
+      break;
+    case "lg":
+      sizeClasses = "px-6 py-3 text-lg";
+      break;
+  }
 
-const Button: React.FC<ButtonProps> = ({ title, size = "md", shape = "rounded-md" }) => {
   return (
-    <button
-      className={`bg-blue-500 text-white ${sizeClasses[size]} ${shape}`}
-    >
+    <button className={`bg-blue-500 text-white ${shape} ${sizeClasses} ${styles}`}>
       {title}
     </button>
   );

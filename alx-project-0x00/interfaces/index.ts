64d4c4ef9feeb9b54ae008d4472c1bdc9548b43a
ifedips20s <ifedips20s@gmail.com>
2025-11-11 12:32:41 +0100

    Add interfaces/index.ts

--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -4,7 +4,9 @@ export interface PillProps {
 
 export interface ButtonProps {
   title: string
-  styles?: string
+  styles: string
+  size?: "sm" | "md" | "lg"
+  shape?: "rounded-sm" | "rounded-md" | "rounded-full"
 }
 
 
